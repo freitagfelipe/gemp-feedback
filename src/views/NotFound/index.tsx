b2782752aa4f6c76ded3be99b4d9a630466d14a2
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-16 03:25:06 -0300

    Update NotFound view style and start using RedirectButton

--- src/views/NotFound/index.tsx
+++ src/views/NotFound/index.tsx
@@ -1,14 +1,15 @@
 import Error from "../../components/Error";
-import { Link } from "react-router-dom";
+import RedirectButton from "../../components/RedirectButton";
 import style from "./style.module.scss";
 
 export default function NotFound() {
     return (
         <div className={style.container}>
-            <Error reason="Página não encontrada" />
-            <Link to="/" className={style.link}>
-                Home
-            </Link>
+            <Error reason="Página não encontrada!" />
+
+            <p>Clique no botão abaixo para ir a tela inicial.</p>
+
+            <RedirectButton to="/" className={style.link}>Home</RedirectButton>
         </div>
     );
 }

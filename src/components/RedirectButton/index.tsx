f3cfdfd47b22f969e842d42ac22b01573e544f1b
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-16 03:24:35 -0300

    Add a RedirectButton component

---
+++ src/components/RedirectButton/index.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import style from "./style.module.scss";
+
+interface Props {
+    to: string;
+    children: string;
+    className?: string;
+}
+
+export default function RedirectButton({ to, children, className }: Props) {
+    return (
+        <Link to={to} className={`${style.link} ${className}`}>
+            { children }
+        </Link>
+    );
+}

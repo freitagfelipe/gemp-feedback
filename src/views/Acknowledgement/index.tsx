bf3542303be2b4662fe620bb8f5e96eee7ced044
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2023-04-18 12:22:49 -0300

    Update Acknowledgement view style

--- src/views/Acknowledgement/index.tsx
+++ src/views/Acknowledgement/index.tsx
@@ -1,17 +1,16 @@
-import Header from "../../components/Header";
 import RedirectButton from "../../components/RedirectButton";
 import style from "./style.module.scss";
 
 export default function Acknowledgement() {
     return (
-        <div className={style.container}>
-            <h2>Obrigado pelo feedback</h2>
+        <div className={style.main_content_container}>
+            <h2 className={style.main_content_container__title}>Obrigado pelo feedback</h2>
 
-            <p>
-                Tentarei ler o mais rápido possível e usá-lo para pensar nos pontos fortes e fracos das nossas aulas. Para voltar a tela inicial basta clicar no botão abaixo.
+            <p className={style.main_content_container__text}>
+                Tentarei ler o seu feedback o mais rápido possível e usá-lo para pensar nos pontos fortes e fracos das nossas aulas. Para voltar a tela inicial basta clicar no botão abaixo.
             </p>
 
-            <RedirectButton to="/">Home</RedirectButton>
+            <RedirectButton to="/">Início</RedirectButton>
         </div>
     );
 }
